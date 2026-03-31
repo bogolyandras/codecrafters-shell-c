8a5eda4aaa245adb0c794fe0299e8d99270e4053
András Bögöly <bogolyandras@protonmail.ch>
2026-03-31 22:40:23 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
 
   printf("$ ");
 
