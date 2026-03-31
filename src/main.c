3a02a957eca04f69cd817cb86cf046f4597de7fc
András Bögöly <bogolyandras@protonmail.ch>
2026-03-31 23:30:15 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NEWLINE "\n"
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
+  while(1) {
+    printf("$ ");
 
-  char command[1024];
-  fgets(command, sizeof(command), stdin);
+    char command[1024];
+    fgets(command, sizeof(command), stdin);
 
-  command[strcspn(command, "\n")] = '\0';
-  printf("%s: command not found\n", command);
+    command[strcspn(command, NEWLINE)] = '\0';
+    printf("%s: command not found\n", command);
+  }
 
   return 0;
 }
