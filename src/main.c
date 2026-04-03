329d4963cd5758de87b44ef9dbe95f542f50d8a9
András Bögöly <bogolyandras@protonmail.ch>
2026-04-03 22:18:02 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,22 +6,42 @@
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
-  setbuf(stdout, NULL);
+  setbuf(stdout, nullptr);
 
   while(1) {
     printf("$ ");
 
     char input[1024];
-    fgets(input, sizeof(input), stdin);
+    char *res = fgets(input, sizeof input, stdin);
+    if(res == NULL) {
+      if (feof(stdin)) {
+        printf("Exiting shell\n");
+        return 0;
+      }
 
-    input[strcspn(input, NEWLINE)] = '\0';
+      perror("fgets");
+      return 1;
+    }
+
+    const size_t newline_index = strcspn(input, NEWLINE);
+    input[newline_index] = '\0';
 
     if (strcmp(input, "exit") == 0) {
       break;
-    } else if (strncmp(input, "echo", 4) == 0) {
-      printf("%s\n", input + 5);
+    } else if (strncmp(input, "echo ", 5) == 0) {
+      printf("%s" NEWLINE, input + 5);
+    } else if (strncmp(input, "type ", 5) == 0) {
+      if (strcmp(input + 5, "echo") == 0) {
+        printf("%s is a shell builtin" NEWLINE, input + 5);
+      } else if (strcmp(input + 5, "exit") == 0) {
+        printf("%s is a shell builtin" NEWLINE, input + 5);
+      } else if (strcmp(input + 5, "type") == 0) {
+        printf("%s is a shell builtin" NEWLINE, input + 5);
+      } else {
+        printf("%s: not found" NEWLINE, input + 5);
+      }
     } else {
-      printf("%s: command not found\n", input);
+      printf("%s: not found" NEWLINE, input);
     }
   }
 
